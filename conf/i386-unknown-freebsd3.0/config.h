7d4f419bcf794e5d36ea847ba0313b9d0e9ca7eb
krueger <krueger>
1998-07-13 13:09:45 +0000

    chan.c conf.c conf.h conf_dial.c config.h d_lock.h msgtailor.c  	s_tailor.c s_tailor.h 	thanks to Mike Muuss <mike@arl.mil> 	3)  freebsd.shar: A SHAR shell archive of the 	conf/i386-unknown-freebsd3.0/ directory. This mostly tracks your 	configuration.  The one important change is that the 	definition of NMSGS in msgtailor.c in the non-pdp11 case needs 	to be increased to something large, like 5000.  This should be 	done for all the provided configurations.

---
+++ conf/i386-unknown-freebsd3.0/config.h
@@ -0,0 +1,75 @@
+/*
+ * $Id: config.h,v 1.2 1998/07/13 13:09:49 krueger Exp $
+ *
+ * please set every configuration-switch here
+ *
+ */
+
+
+#ifndef CONFIG_OLD_H
+#define CONFIG_OLD_H
+
+/*            This makes the channel programs operate inde-
+ *             pendently  from deliver.  It hasn't been used
+ *             for years and requires reading  the  code  to
+ *             understand   its   effect.    Do  NOT  define
+ *             RUNALONE! (it is used for debugging)
+ */
+/* #undef RUNALONE */
+
+/*             Enables code that is specific for 4.2  BSD  .
+ *             All  4.2 BSD sites must define this variable.
+ *             This should be used by 4.3 BSD sites as well.
+ *             4.3   BSD   sites  will  also  want  the  new
+ *             src/smtp/smtpd.4.3.c  for  use  with   INETD.
+ *             They  will  have to compile this manually for
+ *             now (or copy it to smtpd.4.2.c).
+ */
+#define V4_2BSD
+
+/*             Enables  code  that does Bell System V tricks
+ *             (probably also useful for System III  instal-
+ *             lations).  Note: if you are running System V,
+ *             you must  also  append  -Drindex=strrchr  and
+ *             -Dindex=strchr to CFLAGS.
+ */
+#define SYS5
+
+/*             Enables  code  specific to System Vr3.  Note:
+ *             if you have the WIN/TCP code you may need  to
+ *             add  -I/usr/netinclude to CFLAGS and -lnet to
+ *             NETLIBS.
+ */
+/* #undef SYS5r3 */
+
+/*             Define this translation if  your  C  compiler
+ *             doesn't understand voids (e.g. V7).
+ */
+/* #undef -Dvoid=int */
+
+/*             Define CITATION=n to limit (to n) the  number
+ *             of lines of text included in error returns by
+ *             rmail and niftp.  n should be at  least  six,
+ *             if  CITATION  is  defined  at  all.   If  not
+ *             defined, rmail will return the entire message
+ *             and niftp will return the first 500 lines.
+ */
+/* #undef CITATION=n */
+
+/*             Define  DBMCACHE  if  your  version of dbm(3)
+ *             builds databases in core and then dumps  them
+ *             to disk with dbmcachedump().
+ */
+/* #undef DBMCACHE */
+
+/**********************************************************************/
+/*             Enable site depend code
+ */
+
+/*             Enable a small fix to slowdown submitm such that the parent
+ *             could finish readring the pipe when submit exits. This is
+ *             needed on RISC-machines like HP9000s700 an newer.
+ */
+/* #undef SUBMIT_TOO_FAST */
+
+#endif /* CONFIG_OLD_H */
