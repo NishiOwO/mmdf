70fe2aac2cde49f2a1eeeb88c8b5a1a62448ef50
krueger <krueger>
2002-09-30 19:46:04 +0000

    *** empty log message ***

--- local-config.sh
+++ local-config.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
-TARGET="sparc-sun-solaris2.5.1"
+LOG=World.log
+#TARGET="sparc-sun-solaris2.5.1"
 
-OPT="--prefix=/opt"
-OPT=$OPT" --libdir=/opt/mmdf/SRC/lib"
-OPT=$OPT" --includedir=/opt/mmdf/SRC/include"
-OPT=$OPT" --bindir=/opt/local/bin"
-OPT=$OPT" --libexecdir=/opt/mmdf/lib"
+OPT="--prefix=/usr"
+#OPT=$OPT" --libdir=/opt/mmdf/SRC/lib"
+#OPT=$OPT" --includedir=/opt/mmdf/SRC/include"
+#OPT=$OPT" --bindir=/opt/local/bin"
+#OPT=$OPT" --libexecdir=/opt/mmdf/lib"
 
-OPT=$OPT" --mmdf-prefix=/opt/mmdf"
-OPT=$OPT" --mlogdir=/var/spool/mmdf/log"
-OPT=$OPT" --mphsdir=/var/spool/mmdf/phase"
-OPT=$OPT" --mquedir=/var/spool/mmdf/lock/home"
+#OPT=$OPT" --mmdf-prefix=/opt/mmdf"
+#OPT=$OPT" --mlogdir=/var/spool/mmdf/log"
+#OPT=$OPT" --mphsdir=/var/spool/mmdf/phase"
+#OPT=$OPT" --mquedir=/var/spool/mmdf/lock/home"
 # OPT=$OPT" --mchndir="
 # OPT=$OPT" --mtbldir="
 
 OPT=$OPT" --mmdf-debug=1"      # 0,1,2
 # OPT=$OPT" --mmdf-dlog"
 # OPT=$OPT" --mmdf-nodomlit=0"
-# OPT=$OPT" --mmdf-leftdots=1"
+OPT=$OPT" --enable-leftdots"
 # OPT=$OPT" --mmdf-statsort=0"
 # OPT=$OPT" --mmdf-citation=1"
 
-# OPT=$OPT" --with-mmdflogin="
-# OPT=$OPT" --with-mmdfgroup="
+OPT=$OPT" --with-mmdflogin=mmdf"
+OPT=$OPT" --with-mmdfgroup=mmdf"
 # OPT=$OPT" --with-rootlogin="
 
 OPT=$OPT" --with-ldap"
@@ -38,28 +39,28 @@ OPT=$OPT" --with-ldap"
 # OPT=$OPT" --enable-lock_files"
 # OPT=$OPT" --enable-dial"
 #OPT=$OPT" --disable-gcc"
-OPT=$OPT" --disable-fhs"
+# OPT=$OPT" --disable-fhs"
 # OPT=$OPT" --disable-elf"
 
 OPT=$OPT" --enable-esmtp"
-# OPT=$OPT" --enable-esmtp_dsn"
-# OPT=$OPT" --enable-esmtp_8bitmime"
+OPT=$OPT" --enable-esmtp_dsn"
+OPT=$OPT" --enable-esmtp_8bitmime"
 # OPT=$OPT" --enable-wildcard"
 OPT=$OPT" --enable-rbl"
-# OPT=$OPT" --enable-filelist"
+OPT=$OPT" --enable-filelist"
 
 # OPT=$OPT" --disable-badusers"
-# OPT=$OPT" --enable-bboards"
-# OPT=$OPT" --enable-blockaddr"
-# OPT=$OPT" --enable-delay"
+# OPT=$OPT" --enable-bboards"   # need MH or NMH
+OPT=$OPT" --enable-blockaddr"
+OPT=$OPT" --enable-delay"
 # OPT=$OPT" --enable-ean"
 # OPT=$OPT" --disable-list"
-# OPT=$OPT" --enable-niftp"
-# OPT=$OPT" --enable-phone"
-# OPT=$OPT" --enable-pobox"
-# OPT=$OPT" --disable-pop"
-# OPT=$OPT" --enable-prog"
-# OPT=$OPT" --enable-smphone"
+OPT=$OPT" --enable-niftp"
+OPT=$OPT" --enable-phone"
+OPT=$OPT" --enable-pobox"
+# OPT=$OPT" --disable-pop"      # need MH or NMH
+OPT=$OPT" --enable-prog"
+OPT=$OPT" --enable-smphone"
 # OPT=$OPT" --disable-smtp"
 # OPT=$OPT" --disable-uucp"
 
@@ -72,4 +73,8 @@ OPT=$OPT" --enable-rbl"
 OPT=$OPT" --enable-virtual-domains"
 
 echo ./configure $OPT --target=$TARGET $*
-./configure $OPT --target=$TARGET $*
+#./configure $OPT --target=$TARGET $*
+rm -f $LOG
+./configure $OPT $* | tee -a $LOG
+echo ========================= make ===================================>>$LOG
+make 2>&1 | tee -a $LOG
