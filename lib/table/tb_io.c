eb62a8c91d6d128184f07602e69ce1f7dc12a697
krueger <krueger>
2002-10-14 07:57:17 +0000

    For tests (gcc 3.0)

--- lib/table/tb_io.c
+++ lib/table/tb_io.c
@@ -207,9 +207,9 @@ long thepos;
 	case NOTOK:
 	    return (NOTOK);       /* can't open what isn't specified    */
     }
-    switch ((int)(*table -> tb_fp))
+    switch ((int)(table -> tb_fp))
     {                             /* not opened yet                     */
-	case NULL:
+        case 0/*NULL*/:
 	case EOF:
 	    return (NOTOK);
     }
