10615b8221ee6d0936a140e953e90a76761d6bba
Nishi <nishi@nishi.boats>
2024-11-25 21:05:49 +0900

    fixing nerr

--- lib/util/ll_err.c
+++ lib/util/ll_err.c
@@ -4,9 +4,9 @@
 /* Augment ll_io routines to print system call errno value */
 
 #if !HAVE_SYS_ERRLIST_DECL
-#if 0
-extern	int	sys_nerr;
-extern	char	*sys_errlist[];
+#if __GLIBC__
+extern	int		sys_nerr;
+extern	const char	*sys_errlist[];
 #endif
 #endif /* HAVE_SYS_ERRLIST_DECL */
 extern	int	errno;
